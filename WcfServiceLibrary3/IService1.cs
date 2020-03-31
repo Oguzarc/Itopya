c14f03360c44454ddc3ee4416a1b983c352ce537
Oguzarc <50195131+Oguzarc@users.noreply.github.com>
2020-03-31 22:31:04 +0300

    Add files via upload

---
+++ WcfServiceLibrary3/IService1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace GettingStartedLib
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        
+        [OperationContract]
+        bool Kontrol(string name);
+        [OperationContract]
+        bool Kontrolcvv(string name);
+        [OperationContract]
+        bool Kontroldate(string name);
+        [OperationContract]
+        bool Kontrolay(string name);
+    }
+}
