e776d0d50ae444baec74c1eadbdf6fb881b0b5d3
Oguzarc <50195131+Oguzarc@users.noreply.github.com>
2020-05-25 21:43:32 +0200

    Add files via upload

---
+++ İtopyaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("İtopyaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("İtopyaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53882332-18d6-44d5-8f69-3bf4693d60c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ İtopyaTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GettingStartedLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace İtopyaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestControl()
+        {
+            var kontrol = new CalculatorService();
+            var sonuc = kontrol.Kontrol("1234567890");
+            Assert.IsTrue(sonuc);
+        }
+        [TestMethod]
+        public void Testcvv()
+        {
+            var kontrol = new CalculatorService();
+            var sonuc = kontrol.Kontrolcvv("545");
+            Assert.IsTrue(sonuc);
+        }
+        [TestMethod]
+        public void TestAy()
+        {
+            var kontrol = new CalculatorService();
+            var sonuc = kontrol.Kontrolay("2");
+            Assert.IsTrue(sonuc);
+        }
+        [TestMethod]
+        public void TestDate()
+        {
+            var kontrol = new CalculatorService();
+            var sonuc = kontrol.Kontroldate("19");
+            Assert.IsTrue(sonuc);
+        }
+
+
+    }
+}
