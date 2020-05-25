f3a846f5decde8416d203c60c113952c68f256e2
Oguzarc <50195131+Oguzarc@users.noreply.github.com>
2020-05-25 21:40:57 +0200

    Add files via upload

---
+++ WcfServiceLibrary3/Service1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace GettingStartedLib
+{
+    public class CalculatorService : ICalculator
+    {   
+        public bool Kontrol(string name)
+        {
+            string KartNo =  "123456789" ;
+
+            Console.WriteLine("Girilen KartNo :" + name);
+            if(name==KartNo )
+            {
+                Console.WriteLine("Kartno Doğru");
+                return true;
+            }
+            Console.WriteLine("Kart No yanlıs");
+            return false;
+        }
+
+        public bool Kontrolay(string name)
+        {
+            string ay = "2";
+
+            Console.WriteLine("Girilen ay :" + name);
+            if (name == ay)
+            {
+                Console.WriteLine("ay Doğru");
+                return true;
+            }
+            Console.WriteLine("ay yanlıs");
+            return false;
+        }
+
+        public bool Kontrolcvv(string name)
+        {
+            string KartNo = "545";
+
+            Console.WriteLine("Girilen cvv :" + name);
+            if (name == KartNo)
+            {
+                Console.WriteLine("CVV Doğru");
+                return true;
+            }
+            Console.WriteLine("CVV yanlıs");
+            return false;
+        }
+
+        public bool Kontroldate(string name)
+        {
+            string tarih = "19";
+
+            Console.WriteLine("Girilen tarih :" + name);
+            if (name == tarih)
+            {
+                Console.WriteLine("tarih Doğru");
+                return true;
+            }
+            Console.WriteLine("tarih yanlıs");
+            return false;
+        }
+    }
+}
